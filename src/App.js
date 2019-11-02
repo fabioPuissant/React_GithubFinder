86352c45e1ff4fc4d5d96217f08aef5f9d1f1cd7
Fabio_Puissant <fabio.pPuissant@hotmail.com>
2019-11-02 11:36:02 +0100

    Before refactoring

--- src/App.js
+++ src/App.js
@@ -1,13 +1,115 @@
-import React from "react";
+import React, { Fragment, Component } from 'react';
+import Navbar from './components/layout/Navbar';
+import './App.css';
+import Users from './components/users/Users';
+import User from './components/users/User';
+import axios from 'axios';
+import Search from './components/users/Search';
+import Alert from './components/layout/Alert';
+import About from './components/pages/About';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import "./App.css";
+class App extends Component {
+  state = {
+    users: [],
+    user: {},
+    repos: [],
+    loading: false,
+    alert: null
+  };
 
-function App() {
-  return (
-    <div className='App'>
-      <h1>Hello from React</h1>
-    </div>
-  );
+  // Fetch all users from GitHub API.
+  // async componentDidMount() {
+  //   this.setState({ loading: true });
+
+  //   const resp = await axios.get(
+  //     `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+  //   );
+  //   this.setState({ users: resp.data, loading: false });
+  // }
+
+  // Search GitHub users.
+  searchUsers = async text => {
+    this.setState({ loading: true });
+    const resp = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    this.setState({ users: resp.data.items, loading: false });
+  };
+
+  //Clear Users
+  clearUsers = () => this.setState({ users: [], loading: false });
+
+  // Alert when nothing inputed
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } });
+    setTimeout(() => this.setState({ alert: null }), 4000);
+  };
+
+  // Get single GitHub user
+  getUser = async username => {
+    this.setState({ loading: true });
+    const resp = await axios.get(
+      `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    this.setState({ user: resp.data, loading: false });
+  };
+
+  // Get user repos
+  getUserRepos = async username => {
+    this.setState({ loading: true });
+    const resp = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    this.setState({ repos: resp.data, loading: false });
+  };
+
+  foo = () => 'bar';
+  render() {
+    const { users, user, loading, repos } = this.state;
+    return (
+      <Router>
+        <div className='App'>
+          <Navbar />
+          <div className='container'>
+            <Alert alert={this.state.alert} />
+            <Switch>
+              <Route
+                exact
+                path='/'
+                render={props => (
+                  <Fragment>
+                    <Search
+                      searchUsers={this.searchUsers}
+                      clearUsers={this.clearUsers}
+                      showClear={users.length > 0 ? true : false}
+                      setAlert={this.setAlert}
+                    />
+                    <Users loading={loading} users={users} />
+                  </Fragment>
+                )}
+              />
+              <Route exact path='/about' component={About} />
+              <Route
+                exact
+                path='/user/:login'
+                render={props => (
+                  <User
+                    {...props}
+                    getUser={this.getUser}
+                    user={user}
+                    loading={loading}
+                    getUserRepos={this.getUserRepos}
+                    repos={repos}
+                  />
+                )}
+              />
+            </Switch>
+          </div>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
